2f0583f98bb3436134f8f905a68c64ef8cde89f3
Fran Gabarda <frangabardaortiz@gmail.com>
2024-02-16 04:01:58 +0100

    vista-principal.fxml terminado ControladorPrincipal construido, falta darle funcionalidad.

--- GestionInventarioInstituto/src/main/java/es/beatkapo/gestioninventarioinstituto/ControladorPrincipal.java
+++ GestionInventarioInstituto/src/main/java/es/beatkapo/gestioninventarioinstituto/ControladorPrincipal.java
@@ -1,14 +1,59 @@
 package es.beatkapo.gestioninventarioinstituto;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class ControladorPrincipal {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox<?> comboBox;
+
+    @FXML
+    private Button editarButton;
+
+    @FXML
+    private Button eliminarButton;
+
+    @FXML
+    private Button guardarButton;
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private TextField marcaText;
+
+    @FXML
+    private TextField modeloText;
+
+    @FXML
+    private TextField precioText;
+
+    @FXML
+    private Button printButton;
+
+    @FXML
+    void editar(ActionEvent event) {
+
     }
+
+    @FXML
+    void eliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void print(ActionEvent event) {
+
+    }
+
 }
